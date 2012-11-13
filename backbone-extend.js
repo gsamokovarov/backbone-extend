da86b53bad6132abaeb1a54727c780c26e6ac5fc
Genadi Samokovarov <gsamokovarov@gmail.com>
2012-11-14 00:19:37 +0200

    Add plugins support

--- backbone-extend.js
+++ backbone-extend.js
@@ -3,8 +3,14 @@
   // Expose the overridable self-propagating extend function.
   Backbone.extend = Backbone.Model.extend;
 
+  // Find all of the extendables types in Backbone. This will cause types
+  // introduced by plugins to use our generalized extend mechanism.
+  var extendables = _.chain(Backbone).functions().filter(function(func) {
+    return Backbone[func].extend === Backbone.extend;
+  }).value();
+
   // Set up inheritance to use the exposed extend so it can be overriden.
-  _.each(['Model', 'Collection', 'Router', 'View', 'History'], function(kind) {
+  _.each(extendables, function(kind) {
     Backbone[kind].extend = function() {
       return Backbone.extend.apply(this, arguments);
     };
